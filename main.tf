6dbaae5b37bb604746848dbef917913f64b15a9c
Batheiah27 <143469116+Batheiah27@users.noreply.github.com>
2025-10-21 19:13:43 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+region = "us-east-1"
+}
+resource "aws_vpc" "one" {
+cidr_block = "10.0.0.0/16"
+tags = {
+Name = "terraform-vpc"
+}
+}
+resorce "aws_subnet" "two" {
+cidr_block = "10.0.0.0/16"
+vpc_id = aws_vpc.one.id
+availability_zone = "us-east-1a" 
+tags = {
+Name = "terraform-subnet"
+}
+}
+
+resource "aws_instance" "three" {
+ami = "ami-0341d95f75f311023"
+instance_type = "t2.micro"
+subnet_id = aws_subnet.two.id
+tags = {
+Name = "terraform-server"
+}
+}
+
