cac4c65051c46aa77d605dd7108341049cd0433f
Batheiah27 <143469116+Batheiah27@users.noreply.github.com>
2025-10-22 17:20:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ Name = "terraform-vpc"
 resorce "aws_subnet" "two" {
 cidr_block = "10.0.0.0/16"
 vpc_id = aws_vpc.one.id
-availability_zone = "us-east-1b" 
+availability_zone = "us-east-1a" 
 tags = {
 Name = "terraform-subnet"
 }
