2447b50dd126ea816b1329b166c7e35751f25c81
Batheiah27 <143469116+Batheiah27@users.noreply.github.com>
2026-02-19 14:15:24 +0530

    Fix instance_type assignment in main.tf

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ Name = "${terraform.workspace}"
 resource "aws_instance" "three" {
 ami = "ami-0f3caa1cf4417e51b"
 count = var.instance_count
-instance_type = "var.instance_type"
+instance_type = var.instance_type
 tags = {
 Name = "${terraform.workspace}"
 }
